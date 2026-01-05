ca3898768cb51ccb2a1780df3a7c8af90400b75d
KandukuriAmar <2200030959@kluniversity.in>
2026-01-05 18:01:29 +0530

    port configuration changed

--- serverQrCode.js
+++ serverQrCode.js
@@ -60,6 +60,8 @@ app.post("/validate-payment", (req, res) => {
     }
 })
 
-app.listen(5000, () => {
-    console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log("Server started on port ", PORT);
 })
